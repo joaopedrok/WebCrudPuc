72007681a31ce4b6e1bce1e76db193de0fe5aec6
Felipe Lima <talktofill@gmail.com>
2015-11-17 22:25:25 -0200

    Contador de usuarios, refatoracao de modulos JS, adicionado thumbnail, melhorias na usabilidade

--- js/tools.js
+++ js/tools.js
@@ -1,11 +1,17 @@
 $(function() {
-	//Executa quando clicar em botao de editar usuario
+	// Cria objeto de controle da pagina
+	var indexCtrl = new indexController();
+
+	// Executa quando clicar em botao de editar usuario
 	$('.btn-create').on('click', function(e) {
 		e.preventDefault();
-		alert('criar');
+		var qty = parseInt($('#users_qty').val());
+		if (qty > 0 ) {
+			indexCtrl.createUsersList(qty);
+		}
 	});
 
-	//Executa quando clicar em botao de editar usuario
+	// Executa quando clicar em botao de editar usuario
 	$('.btn-edit').on('click', function(e) {
 		e.preventDefault();
 
@@ -13,56 +19,84 @@ $(function() {
 		alert('Editar usuario de ID ' + $row.data('userid'));
 	});
 
-	//Executa quando clicar em botao de remover usuario
-	$('.btn-remove').on('click', function(e) {
+	// Executa quando clicar em botao de remover usuario
+	$('#users_list').on('click', '.btn-remove', function(e) {
 		e.preventDefault();
-
-		var $row = $(this).parent().parent();
-		alert('Remover usuario de ID ' + $row.data('userid'));
+		indexCtrl.removeUser(this);
 	});
+
+	// Executa quando clica Enter dentro do input de qty de usuarios
+	$('#form_create_users').on('submit', function(e) {
+		e.preventDefault();
+		var qty = parseInt($('#users_qty').val());
+		if (qty > 0 ) {
+			indexCtrl.createUsersList(qty);
+		}
+	});
+
 });
 
-var isParentOdd = function(){
-	return $(".row").length % 2 == 0 ? '' : ' odd';
-}
+function indexController(){
 
-var templateUsers = function() {
-	var template = 
-	'{{#results}}' +
-	'<div class="row' + isParentOdd() + '" data-userid="{{ registered }}">' +
-		'<div class="col-xs-8">' +
-			'<span>{{ user.name.first }} {{ user.name.last }}</span>' +
-		'</div>' +
-		'<div class="col-xs-4 text-right">' +
-			'<a href="#" class="button btn-edit" title="Editar"><i class="fa fa-pencil-square-o"></i> Editar</a>' +
-        	'<a href="#" class="button btn-remove" title="Remover"><i class="fa fa-times"></i> Excluir</a>' +
-		'</div>' +
-	'</div>' +
-	'{{/results}}';
-	return template;
-}
+	// Adiciona json de usuario no array
+	this.setUser = function(user) {
+		//TODO Salvar usuario no banco
+		this.addListRow(user);
+		this.updateUsersCount();
+	};
 
-var qtUsers = function(){
-	return parseInt($('qtUsers').text().trim());
-}
+	// Remove usuario da lista
+	this.removeUser = function(element) {
+		//TODO Remove usuario no banco
 
-for (var i = 0; i < qtUsers; i++) {
-	$.ajax({
-	url: 'https://randomuser.me/api/',
-	dataType: 'json',
-	// data: '', // to send to 
-	success: function(data) {
-		//console.dir(data);
-		console.log(isParentOdd());
-		/* global Mustache */
-		var rendered = Mustache.render(templateUsers(), data);
-		$('#users_list').append(rendered);
-	}
-});
-}
+		var $row = $(element).parent().parent();
+		$row.addClass('removed-item');
 
-// proper case string prptotype (JScript 5.5+)
-String.prototype.toProperCase = function() {
-  return this.toLowerCase().replace(/^(.)|\s(.)/g, 
-      function($1) { return $1.toUpperCase(); });
-};
+		window.setTimeout(function(){ $row.remove()}, 600);
+		this.updateUsersCount();
+	};
+
+	// Cria lista de usuarios nova
+	this.createUsersList = function(usersQty) {
+		this.createUserFromAPI(usersQty);
+	};
+
+	// Cria novo usuario da API Random User
+	this.createUserFromAPI = function(usersQty) {
+		var self = this;
+
+		$.ajax({
+			url: 'http://api.randomuser.me/?results='+usersQty,
+			dataType: 'json',
+			success: function(data) {
+				for(var i = 0; i < data.results.length; i++) {
+					self.setUser(data.results[i]);
+				}
+			}
+		});
+	};
+
+	// Adiciona linha na tabela de usuarios
+	this.addListRow = function(user) {
+		var tableRow = Mustache.render(this.rowTemplate(), user);
+		$('#users_list').append(tableRow);
+	};
+
+	this.updateUsersCount = function() {
+		var users = $('#users_list').find('.row').length;
+		$('#users_count').html(users + ' usuário(s)');
+	};
+
+	// Layout da linha da tabela
+	this.rowTemplate = function() {
+		return '<div class="row" data-userid="{{ user.registered }}">' +
+			'<div class="col-xs-8">' +
+				'<img src="{{ user.picture.thumbnail }}" class="rounded-face"><span>{{ user.name.first }} {{ user.name.last }}</span>' +
+			'</div>' +
+			'<div class="col-xs-4 actions">' +
+				'<a href="#" class="button btn-edit" title="Editar"><i class="fa fa-pencil-square-o"></i> Editar</a>' +
+	        	'<a href="#" class="button btn-remove" title="Remover"><i class="fa fa-times"></i> Excluir</a>' +
+			'</div>' +
+		'</div>';
+	};
+}
